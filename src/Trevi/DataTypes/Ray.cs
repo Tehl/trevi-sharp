5b48394b1ca0eef4fb9036212c04606489deb41d
Tehl <Tehl@users.noreply.github.com>
2023-07-23 19:28:03 +0100

    Add Vertex, Ray data types

---
+++ src/Trevi/DataTypes/Ray.cs
@@ -0,0 +1,45 @@
+﻿namespace Trevi.DataTypes;
+
+/// <summary>
+///     Ray Data Type
+/// </summary>
+internal class Ray
+{
+    /// <summary>
+    ///     Constructor
+    /// </summary>
+    public Ray(Vector position, Vector direction)
+    {
+        Position = position;
+        Direction = direction;
+    }
+
+    /// <summary>
+    ///     Default constructor
+    /// </summary>
+    public Ray()
+    {
+        Position = new Vector(0, 0, 0);
+        Direction = new Vector(0, 0, 1);
+    }
+
+    /// <summary>
+    ///     Copy constructor
+    /// </summary>
+    public Ray(Ray source)
+    {
+        Position = new Vector(source.Position);
+        Direction = new Vector(source.Direction);
+    }
+
+    public Vector Position { get; set; }
+    public Vector Direction { get; set; }
+
+    #region Member Functions
+
+    // TODO: Reflect()
+
+    // TODO: Refract()
+
+    #endregion // Member Functions
+}
---
+++ src/Trevi/DataTypes/Vertex.cs
@@ -0,0 +1,51 @@
+﻿namespace Trevi.DataTypes;
+
+/// <summary>
+///     Vertex Data Type
+/// </summary>
+internal class Vertex
+{
+    /// <summary>
+    ///     Constructor
+    /// </summary>
+    public Vertex(Vector position, Vector normal, double u = 0, double v = 0)
+    {
+        Position = new Vector(position);
+        Normal = new Vector(normal);
+        U = u;
+        V = v;
+    }
+
+    /// <summary>
+    ///     Copy constructor
+    /// </summary>
+    public Vertex(Vertex source)
+    {
+        Position = new Vector(source.Position);
+        Normal = new Vector(source.Normal);
+        U = source.U;
+        V = source.V;
+    }
+
+    /// <summary>
+    ///     Default constructor
+    /// </summary>
+    public Vertex()
+    {
+        Position = new Vector();
+        Normal = new Vector();
+        U = 0;
+        V = 0;
+    }
+
+    public Vector Position { get; set; }
+    public Vector Normal { get; set; }
+    public double U { get; private set; }
+    public double V { get; private set; }
+
+    public void UV(double u, double v)
+    {
+        U = u;
+        V = v;
+    }
+}
